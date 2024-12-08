c1a40bb70ad9723405a8a365c3253fa4c5c9bbea
AlvaroFrancoHackUPC1 <alvaro.franco@estudiantat.upc.edu>
2024-12-08 19:23:02 +0100

    Zombie_v5

--- game/AIZombie.cc
+++ game/AIZombie.cc
@@ -4,7 +4,7 @@
  * Write the name of your player and save this file
  * with the same name and .cc extension.
  */
-#define PLAYER_NAME Zombie_v4
+#define PLAYER_NAME Zombie_v5
 
 struct PLAYER_NAME : public Player {
   /**
